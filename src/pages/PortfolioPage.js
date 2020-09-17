2bcefb81ef000533fde17d2a26686f4f152c87e3
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-09-17 11:43:03 -0400

    added column to allow for particle.js to run behind

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import Hero from '../components/Hero';
-import Carousel from '../components/Carousel';
+import Hero from "../components/Hero";
+import Carousel from "../components/Carousel";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 function PorfolioPage(props) {
-
-    return(
-        <div>
-            <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
-            <Carousel />
-        </div>
-    );
-
+  return (
+    <div>
+      <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
+      <Container className="bg-transparent p-3">
+      <Row className="justify-content-center">
+      <Col>
+        <Carousel />
+      </Col>
+            </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default PorfolioPage;
