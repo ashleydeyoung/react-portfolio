2a7f91e7f62779d46eb8b70fba52036715b59519
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-09-17 11:39:45 -0400

    changed font colors

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -7,14 +7,14 @@ function CardInfo(props) {
     const style = useSpring({opacity: 1, from: {opacity: 0}});
 
     return(
-        <animated.div className="card-info text-center wrap font1" style={style}>
+        <animated.div className="card-info text-center wrap text-white" style={style}>
             <h5 className="card-title font-weight-bolder">{props.title}</h5>
             <div className="card-text font-italic p-2">{props.subTitle}</div>
             <div className="card-text tech">Technologies used:</div>
             <p className="card-text tech">{props.technologies}</p>
-            <a href={props.link}  target="_blank" rel="noopener noreferrer">Deployed Link</a>
+            <a href={props.link}  target="_blank" className= "fontPink" rel="noopener noreferrer">Deployed Link</a>
             <br/>
-            <a href={props.github}  target="_blank" rel="noopener noreferrer">Github</a>
+            <a href={props.github}  target="_blank" className= "fontGreen"  rel="noopener noreferrer">Github</a>
         </animated.div>
     );
 
